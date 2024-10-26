a211ed046a43f0e091d234acd5c1d71f33ce2384
Rick <121335204+rickcomics@users.noreply.github.com>
2024-10-26 21:49:12 +0300

    Update button.js

--- button.js
+++ button.js
@@ -2,5 +2,5 @@ const newButton = document.createElement('button');
 newButton.textContent = 'wallet connect';
 button.classList.add('btn');
 button.setAttribute('type', 'button');
-buttonRootId?: string | null;
+
 document.body.appendChild(newButton);
