b5145d08c35822d242c94db44993dafe70a226cd
Rick <121335204+rickcomics@users.noreply.github.com>
2024-10-26 22:01:15 +0300

    Update button.js

--- button.js
+++ button.js
@@ -2,5 +2,9 @@ const newButton = document.createElement('button');
 newButton.textContent = 'wallet connect';
 button.classList.add('btn');
 button.setAttribute('type', 'button');
+button.addEventListener('click', () => {
+  alert('Oh, you clicked me!')
+})
+
 
 document.body.appendChild(newButton);
