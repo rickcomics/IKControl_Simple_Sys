f885e320eb98cf34d9d75f69439203a48d9bf51e
Rick <121335204+rickcomics@users.noreply.github.com>
2024-10-26 22:18:03 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const btn = document.createElement('btn');
 btn.innerText = "text";
-
+button.addEventListener('click', () => {
+  alert('Oh, you clicked me!')
+})
 document.body.appendChild('btn');
