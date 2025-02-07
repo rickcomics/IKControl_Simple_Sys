d35ce3aa4874930753054cc2fd75b8579a31165b
Rick <121335204+rickcomics@users.noreply.github.com>
2025-02-07 09:01:09 +0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const btn = document.createElement('btn');
-btn.innerText = 'text';
-btn.classList.add('btn');
-btn.addEventListener('click', () => {
-  alert('Oh, you clicked me!')
-})
-document.body.appendChild('btn');
